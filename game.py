d1e00918d525a6ef4983f7536b564fd145e84eb4
James Jenkins <jpycoder@gmail.com>
2020-11-19 02:56:07 -0600

    Add skeleton base class for Game object

---
+++ game.py
@@ -0,0 +1,11 @@
+class Game:
+    def __init__(self, n_players):
+        self.cities = []
+
+    def update(self):
+        # TODO: Implement a single turn
+        pass
+
+    def play(self):
+        # TODO: Loop over turns
+        pass
