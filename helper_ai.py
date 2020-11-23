95bdfad776f6a3a57dd9b97c59969741c26b7e57
spencer watza <s.g.watza@gmail.com>
2020-11-22 19:43:58 -0600

    got working the city outbreak probability... more probability work is needed

--- helper_ai.py
+++ helper_ai.py
@@ -7,17 +7,12 @@ def calculate_uniform_probability_of_card_draw(num_of_cards):
 
 def calculate_chance_of_epidemic(game):
     #Do we cast this as int or round to ceiling?
-    print("Epidemic " + str(game.num_of_epidemics) + "#")
-    print("Starting Cards " + str(game.starting_num_of_player_cards) + "#")
     split_size = float(game.starting_num_of_player_cards) / game.num_of_epidemics
-    print("split size is " + str(split_size))
     cards_left = game.player_cards.remaining_cards()
     cards_burnt = game.starting_num_of_player_cards - cards_left
     #now calculate how many epidemic splits have we gone through
     rem = split_size - cards_burnt % split_size
-    print("Remainder Value " + str(rem))
     expected_epidemics = float(cards_burnt / split_size) + 1
-    print("Expected Epidemics " + str(expected_epidemics))
     if game.occurred_epidemics >= expected_epidemics:
         return 0
     else:
@@ -28,7 +23,7 @@ def calculate_chance_of_epidemic(game):
 
 def calculate_drawing_infection_city_card(game, city):
     p_epidemic = calculate_chance_of_epidemic(game)
-    print("probability of drawing epidemic is " + str(p_epidemic))
+    #print("probability of drawing epidemic is " + str(p_epidemic))
     if check_if_in_discard(game,city.name):
         p_card = 0
         for r in range(game.infection_rate):
@@ -78,10 +73,10 @@ def calculate_drawing_epidemic_infection_city_card(game, city):
 
 def calculate_outbreak_in_city(game, city):
     #for now assume only 1 turn and no mixed color outbreaks;
-    if city.disesase[city.color] == 3:
+    if city.diseases[city.color] == 3:
         p_draw = calculate_drawing_infection_city_card(game, city)
         return p_draw
-    elif city.disesase[city.color] > 1:
+    elif city.diseases[city.color] > 1:
         p_draw = calculate_drawing_epidemic_infection_city_card(game,city)
         return p_draw
     else:
